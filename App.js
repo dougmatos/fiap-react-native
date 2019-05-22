30619c168cd488dc69287b68b0e893d3b77b9249
Usuário Convidado <Guest@PA06MICRO26.local>
2019-05-22 20:40:34 -0300

    commit inicial

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Seasons from './components/Seasons'
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Seasons />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/Seasons/index.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react'
+import { View, Text } from 'react-native'
+
+class Seasons extends PureComponent{
+
+    renderSeasons(){
+        let items = [];
+
+        for(let i =0; i < 19; i++){
+            items.push(<Text key={ `season-${i}` }>{ i.toFixed(2) }</Text>);
+        }
+        return items;
+    }
+
+    render(){
+        return (
+            <View>
+                { this.renderSeasons() }
+            </View>
+        );
+    }
+}
+
+
+export default Seasons;
